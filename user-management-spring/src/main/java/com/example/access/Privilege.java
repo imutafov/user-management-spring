caa585f6d9278b44127d1022716bd730405de68c
imutafov <il.mutafov@gmail.com>
2017-02-08 09:36:22 +0200

    added a public view to display limited data

--- user-management-spring/src/main/java/com/example/access/Privilege.java
+++ user-management-spring/src/main/java/com/example/access/Privilege.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.example.user;
+package com.example.access;
 
+import com.example.user.Role;
 import java.util.Collection;
 import javax.persistence.Column;
 import javax.persistence.Entity;
