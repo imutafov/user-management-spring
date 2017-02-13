5b85cd8617ad25d90e72ce6c52cc6b8db8b3d55b
imutafov <il.mutafov@gmail.com>
2017-02-13 13:50:59 +0200

    added page support

--- user-management-spring/src/main/java/com/example/employee/EmployeeController.java
+++ user-management-spring/src/main/java/com/example/employee/EmployeeController.java
@@ -7,6 +7,7 @@ package com.example.employee;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
@@ -35,8 +36,8 @@ public class EmployeeController {
 
     @RequestMapping(value = "/employees", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(value = HttpStatus.OK)
-    public List<Employee> getAllEmployees() {
-        return service.getAllEmployees();
+    public List<Employee> getAllEmployees(Pageable pageRequest) {
+        return service.getAllEmployees(pageRequest).getContent();
     }
 
     @RequestMapping(value = "/employees/self", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -47,7 +48,7 @@ public class EmployeeController {
         return empl;
     }
 
-    @RequestMapping(value = "/employees/", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/employees", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(value = HttpStatus.OK)
     public EmployeeDTO update(@RequestBody EmployeeDTO empl) throws Exception {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
