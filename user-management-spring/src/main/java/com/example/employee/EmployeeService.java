9271c7281736f99f43a9b47a04ac9c2c27ed4033
Teodor Todorov <banskaliqtabg@gmail.com>
2017-02-17 14:19:38 +0200

    To create an employee there is no need of existing user. Now user is created along with the employee

--- user-management-spring/src/main/java/com/example/employee/EmployeeService.java
+++ user-management-spring/src/main/java/com/example/employee/EmployeeService.java
@@ -7,6 +7,7 @@ package com.example.employee;
 
 import com.example.user.User;
 import com.example.user.UserRepository;
+import com.example.user.UserService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -19,40 +20,49 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class EmployeeService {
-    
+
     @Autowired
     private EmployeeRepository repo;
-    
+
     @Autowired
     private UserRepository userRepo;
-    
-    public Employee save(Employee empl, Long id) {
-        User user = userRepo.findByUserId(id);
-        empl.setUser(user);
+
+    @Autowired
+    private UserService userService;
+
+    public Employee save(Employee empl) {
+        User user = empl.getUser();
+        userService.saveUser(user);
+
+        user.setBirthDate(empl.getDob());
+        user.setFirstName(empl.getFirstName());
+        user.setLastName(empl.getLastName());
+        user.setPhoneNumber(empl.getPhoneNumber());
+
         return repo.save(empl);
     }
-    
+
     public Employee getById(Long id) {
         return repo.findOne(id);
     }
-    
+
     public List<Employee> getAllEmployees() {
         return repo.findAll();
     }
-    
+
     public Employee getByUsername(String username) {
         return repo.findByUserUserName(username);
     }
-    
+
     public Page<EmployeeDTO> getAllEmployees(Pageable pageRequest) {
         Page<Employee> resultPage = repo.findAll(pageRequest);
         return EmployeeMapper.mapEntityPageIntoDTOPage(pageRequest, resultPage);
     }
-    
+
     public Page<Employee> getEmployeesByEmployer(String employerName, Pageable pageRequest) {
         return repo.findByEmployerUserUserName(employerName, pageRequest);
     }
-    
+
     public Employee update(Long id, Employee empl) throws Exception {
         Employee dbEmpl = repo.findOne(id);
         if (empl == null) {
@@ -65,39 +75,39 @@ public class EmployeeService {
         dbEmpl.setCommission(empl.getCommission());
         return repo.save(dbEmpl);
     }
-    
+
     public EmployeeDTO getEmployeeDTOByUserName(String userName) {
         Employee empl = repo.findByUserUserName(userName);
         return EmployeeMapper.mapEntityIntoDTO(empl);
     }
-    
+
     public List<EmployeeTaskDTO> getAllTasked() {
         List<Employee> employees = repo.findAll();
         return EmployeeTaskMapper.mapEntitiesIntoDTOs(employees);
     }
-    
+
     public EmployeeDTO update(String userName, EmployeeDTO employee) throws Exception {
         Employee empl = repo.findByUserUserName(userName);
         if (empl == null) {
             throw new Exception("User not found");
         }
-        
+
         empl.setFirstName(employee.getFirstName());
         empl.setMiddleName(employee.getMiddleName());
         empl.setLastName(employee.getLastName());
         empl.setDob(employee.getDob());
         empl.setSex(employee.getSex());
         empl.setPhoneNumber(employee.getPhoneNumber());
-        
+
         repo.save(empl);
         return getEmployeeDTOByUserName(userName);
     }
-    
+
     public Employee changeActive(Long id) {
         Employee empl = repo.findOne(id);
         empl.getUser().setEnabled(!empl.getUser().isEnabled());
         repo.save(empl);
         return empl;
     }
-    
+
 }
